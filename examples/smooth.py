38b2594ba27886cd34819746af7ded88129ae222
Phil Howard <phil@gadgetoid.com>
2016-12-07 12:15:14 +0000

    Removed remaining LEDs stuff from smooth.py

--- examples/smooth.py
+++ examples/smooth.py
@@ -10,12 +10,6 @@ while True:
     # Get the time in seconds
     t = time.time()
 
-    # Generate a sine wave brightness transition from 0 to 1
-    b = (math.sin(t) + 1) / 2
-
-    # Convert brightness to 0 to 255
-    b = int(b * 255.0)
-
     # G enerate an angle using a sine wave (-1 to 1) multiplied by 90 (-90 to 90)
     a = math.sin(t * 2) * 90
 
@@ -26,4 +20,4 @@ while True:
     print(round(a,2))
 
     # Sleep for a bit so we're not hammering the HAT with updates
-    time.sleep(0.01)
+    time.sleep(0.005)
